9edc952882f5dae6c83a080564e0cc7548093095
KiranKumarpadhy <55530244+KiranKumarpadhy@users.noreply.github.com>
2022-01-04 12:06:34 +0530

    Add files via upload

---
+++ HeraizenAssignment3quention4/LowSalException.java
@@ -0,0 +1,15 @@
+package com.HeraizenAssignmentNo3.QuestionNo4;
+
+public class LowSalException extends Exception {
+
+	public LowSalException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public LowSalException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
